6da374381d3fbab0826f367f11c7a736f76271dc
mituya-hiroki <soccer.g.osaka.11@gmail.com>
2020-05-29 18:26:23 +0900

    finish

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,3 +13,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 Rails.application.config.assets.precompile += %w( home.css )
+Rails.application.config.assets.precompile += %w( show.css )
+Rails.application.config.assets.precompile += %w( edit.css )
+Rails.application.config.assets.precompile += %w( index.css )
+
